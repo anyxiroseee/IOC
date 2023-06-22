6e864efe096cd22f72a49cd06a20f1eb4982f9a8
Nastya F <fedorina.nastya@mail.ru>
2023-06-23 01:22:36 +0700

    Конкретные стратегии
    Стратегии AMDprocessor и Intelprocessor

--- amdprocessor.h
+++ amdprocessor.h
@@ -1,4 +1,28 @@
 #ifndef AMDPROCESSOR_H
 #define AMDPROCESSOR_H
+#include"iostream"
+#include"Processor.h"
+
+class AMDProcessor: public IProcessor
+{
+     std::string Version;
+     ProcessorType Type;
+     double Speed;
+
+public:
+     void setProcessor(std::string new_version, ProcessorType new_type, double new_speed) override{
+         Version = new_version;
+         Type = new_type;
+         Speed = new_speed;
+     }
+
+     AMDProcessor() = default;
+     ~AMDProcessor() override{ std::cout<<"destructor amd"<< std::endl;};//наследники амд и интел реализуют вирт методы интерфейса - гет и сет
+
+    std::string getProcessorInfo() override{
+        return "Processor AMD version: " + Version + ", type:  x" + std::to_string(Type) + ", speed: " + std::to_string(Speed);
+    }
+
+ };
 
 #endif // AMDPROCESSOR_H
