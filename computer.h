7927baef01051e38bc33ed327caac146360bf823
Nastya F <fedorina.nastya@mail.ru>
2023-06-23 01:21:32 +0700

    Контекст computer

--- computer.h
+++ computer.h
@@ -1,4 +1,22 @@
 #ifndef COMPUTER_H
 #define COMPUTER_H
+#include<iostream>
+#include<memory>
+#include "Processor.h"
+
+using namespace std;
+
+class Computer
+{
+    std::shared_ptr<IProcessor> processor;
+public:
+    Computer(std::shared_ptr<IProcessor> p): processor(p) {} // конструктор инициализирует процессор
+
+    string getProcessor()
+    {
+       return processor->getProcessorInfo();
+    }
+
+};
 
 #endif // COMPUTER_H
